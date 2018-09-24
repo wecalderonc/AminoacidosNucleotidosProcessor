9434ba4ddbd036729be01164977d4df3e74f3838
WIlliam Calderon <wec999@gmail.com>
2018-09-24 13:40:18 -0500

    Added maincomplete to convert all the phy files in the folder

---
+++ maincomplete.rb
@@ -0,0 +1,62 @@
+require 'csv'
+require 'pry'
+
+
+files = Dir["*.phy"]
+# filepath = 'test.phy'
+ordenColumnas = Array.new(513) {Array.new(4,0)}
+
+files.each do |filepath|
+  especieCadenaArray = []
+  control = []
+  temp = []
+  tempN = []
+
+
+  #Guarda cada línea del texto en un array y los separa entre título y cadena de nucleótidos
+  File.readlines(filepath).each do |line|
+    especieCadenaArray << line.split(' ')
+  end
+
+  #Guarda cadena de control y divide cada letra en una posíción del array
+  control = especieCadenaArray[0][1].split(//)
+
+  especieCadenaArray.each do |array|  #itero dentro de todo el array de arrays
+    temp = array[1].split(//) #dividido cada cadena con el split y lo guardo en temp
+    # temp2 = tempAnterior[1].split(//) #dividido cada cadena del array anterior
+    x = 0
+    tempN << array[0]
+    temp.each do |letter|     #itero dentro de temp para comparar con el control
+      if ordenColumnas[x].include? (letter)
+        if letter == control[x]
+          tempN << 0
+        elsif letter == "-"
+          tempN << "-"
+        else
+          tempN << ordenColumnas[x].index(letter) - 4
+        end
+      else
+        ordenColumnas[x] << letter
+        if letter == control[x]
+          tempN << 0
+        elsif letter == "-"
+          tempN << "-"
+        else
+          tempN << ordenColumnas[x].index(letter) - 4
+        end
+      end
+      tempN.length()
+      x = x + 1
+    end
+    # puts array[0] #Nombre especie
+    p tempN #Cadena 0y1s
+
+    CSV.open("#{filepath}.csv", "a+") do |csv|
+      csv << tempN
+    end
+
+    puts tempN.length() #Largo de la cadena
+    tempN = []
+   # puts "************************************"
+ end
+end
